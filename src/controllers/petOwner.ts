9589c9a2f4dba9d8fe3fb0a5bdfc8fbe1c983cc9
Tong Lin <wowawawoa@gmail.com>
2023-03-27 15:39:13 +1030

    chore: update get pet owner result

--- src/controllers/petOwner.ts
+++ src/controllers/petOwner.ts
@@ -18,6 +18,9 @@ export const getPetOwner: RequestHandler = async (req, res, next) => {
     const petOwner = await PetOwner.findById(petOwnerId).populate({
       path: "petSitter",
       strictPopulate: false,
+    }).populate({
+      path: "pets",
+      strictPopulate: false,
     });
     if (!petOwner || !petOwner.isActive) {
       throw createHttpError(400, "Pet owner not found");
